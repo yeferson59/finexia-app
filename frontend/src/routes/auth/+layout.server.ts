b1f4d8adbec8c8ac77cd4172429143f08d6cca50
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-19 23:43:33 -0500

    feat: redirect authenticated users from auth layout

---
+++ frontend/src/routes/auth/+layout.server.ts
@@ -0,0 +1,8 @@
+import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+	if (locals.session && locals.user) {
+		return redirect(303, '/dashboard');
+	}
+};
